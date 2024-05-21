b5fa40e80823050ec2dae1eaa60c88837840db0b
Maloș Mihai <malosmihai1@yahoo.com>
2024-05-21 12:08:03 +0300

    small dataset for testing

--- FakeNewsDetection/DataFiltering.py
+++ FakeNewsDetection/DataFiltering.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 fake_dataset = pd.read_csv('Fake.csv')
-fake_dataset['Label'] = '0'
+fake_dataset['label'] = '0'
 
 true_dataset = pd.read_csv('True.csv')
-true_dataset['Label'] = '1'
+true_dataset['label'] = '1'
 
 # Merge the two CSV files
 full_dataset = pd.concat([fake_dataset, true_dataset], ignore_index=True)
