bf307d889ed39467b907f5b4fa81728afb44a44b
Maloș Mihai <malosmihai1@yahoo.com>
2024-05-21 18:54:23 +0300

    Data url and non-ascii filter added

--- FakeNewsDetection/DataFiltering.py
+++ FakeNewsDetection/DataFiltering.py
@@ -1,9 +1,37 @@
 import pandas as pd
+import re
+
+found_links = []
+
+def remove_non_ascii_words(text):
+    ascii_word_pattern = re.compile(r'[^\x00-\x7F]+')
+    ascii_words = ascii_word_pattern.sub('', text)
+    return ascii_words
+
+def remove_urls(text):
+    url_pattern = re.compile(r'http[s]?://\S+')
+    urls = url_pattern.findall(text)
+    found_links.extend(found_links)
+    return url_pattern.sub('', text)
+
+def clean_text(text):
+    text = remove_non_ascii_words(text)
+    text = remove_urls(text)
+    return text
+
+def clean_dataset(dataset):
+    cleaned_dataset = dataset.map(lambda x: clean_text(str(x)))
+    cleaned_dataset = cleaned_dataset.replace('', pd.NA).dropna(how='all')
+    return cleaned_dataset
+
 
 fake_dataset = pd.read_csv('Fake.csv')
-fake_dataset['label'] = '0'
-
 true_dataset = pd.read_csv('True.csv')
+
+fake_dataset = clean_dataset(fake_dataset)
+true_dataset = clean_dataset(true_dataset)
+
+fake_dataset['label'] = '0'
 true_dataset['label'] = '1'
 
 # Merge the two CSV files
@@ -11,3 +39,7 @@ full_dataset = pd.concat([fake_dataset, true_dataset], ignore_index=True)
 
 # Save the merged CSV to a new file
 full_dataset.to_csv('FakeReal.csv', index=False)
+
+with open('removed_urls.txt', 'a') as f:
+    for url in found_links:
+        f.write(url + '\n')
--- FakeNewsDetection/ModelInference.py
+++ FakeNewsDetection/ModelInference.py
@@ -2,6 +2,7 @@ from transformers import RobertaTokenizer, RobertaModel
 import torch
 from torch import cuda
 from tqdm import tqdm
+from torchinfo import summary
 device = 'cuda' if cuda.is_available() else 'cpu'
 
 class RobertaClass(torch.nn.Module):
@@ -22,6 +23,7 @@ class RobertaClass(torch.nn.Module):
         output = self.classifier(pooler)
         return output
 
+
 # Load the tokenizer
 tokenizer = RobertaTokenizer.from_pretrained("./model/tokenizer")
 
@@ -30,10 +32,9 @@ model = RobertaClass()
 model.load_state_dict(torch.load('./model/fake_news.bin'))
 model.to(device)
 model.eval()
+summary(model)
 
-sentence = "(Reuters) - A lottery drawing to settle a tied Virginia legislative race that could shift the statehouse balance of power has been indefinitely postponed, state election officials said on Tuesday, after the Democratic candidate mounted a legal fight. The decision to put off the high-stakes lotto, originally scheduled for Wednesday, marks the latest twist in a dramatic election recount that at one point showed Democrat Shelly Simonds beating Republican incumbent David Yancey by a single vote.  A victory by Simonds would shift Republicansâ€™ slim control of the 100-member House of Delegates to an even 50-50 split with the Democrats, forcing the two parties into a rare power-sharing arrangement. A day after Simonds emerged as the victor of a recount, a three-judge panel ruled that a disputed ballot should be counted for Yancey. That decision left the two candidates tied with 11,608 votes each in a district that encompasses the shipping hub of Newport News in southeastern Virginia, setting the stage for the equivalent of a coin toss to pick a final winner. Simonds asked a state court to reconsider on Tuesday, arguing that the disputed ballot was wrongly included. An image filed in court showed that the ballot had bubbles filled in beside both names, with a slash mark by Simondsâ€™ name. The voter selected Republicans for other offices. Simonds told reporters that the case had implications not only for her contest but for the integrity of state elections as a whole, saying that without a court ruling in her favor, â€œrecounts would become a never-ending spiral of courtroom challenges.â€ The chairman of the Virginia Board of Elections, James Alcorn, said in a statement that while holding a lottery would be in keeping with state law, such a move should be considered â€œan action of last resort.â€ He added: â€œAny substantive concerns regarding the election or recount should be resolved before a random drawing is conducted.â€    Yanceyâ€™s campaign did not immediately respond to requests for comment. The Virginia House Republican Caucus said in a statement that it was reviewing the new court filings. â€œWe believe the court acted appropriately and that the integrity of the process is without question,â€ spokesman Parker Slaybaugh said. Virginia Department of Elections spokeswoman Andrea Gaines said in an email that no new date for a drawing has been set"
-
-
+sentence = ""
 inputs = tokenizer(sentence, return_tensors="pt").to(device)
 with torch.no_grad():
     data = inputs
